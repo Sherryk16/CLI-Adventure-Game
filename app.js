87432f6526a26693b8d2dc89bfa9cc51b6e068f9
Sheharyar Khan <159763759+Sherryk16@users.noreply.github.com>
2024-05-10 22:15:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,159 @@
+import inquirer from 'inquirer';
+class Player {
+    name;
+    fuel = 100;
+    constructor(name) {
+        this.name = name;
+    }
+    fuelDecrease() {
+        let fuel = this.fuel - 10;
+        this.fuel = fuel;
+    }
+}
+class Opponent {
+    name;
+    fuel = 100;
+    constructor(name) {
+        this.name = name;
+    }
+    fuelDecrease() {
+        let fuel = this.fuel - 10;
+        this.fuel = fuel;
+    }
+}
+let player = await inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+]);
+let opponet = await inquirer.prompt([
+    {
+        type: 'list',
+        name: 'select',
+        message: 'Select Your Opponent',
+        choices: ['King', 'Paul', 'Lee']
+    }
+]);
+let p1 = new Player(player.name);
+let p2 = new Opponent(opponet.select);
+do {
+    //King
+    if (opponet.select == 'King') {
+        let ask = await inquirer.prompt([
+            {
+                name: 'opt',
+                type: 'list',
+                message: 'What do you want to do?',
+                choices: ['Attack', 'Surrender']
+            }
+        ]);
+        if (ask.opt == 'Attack') {
+            let num = Math.floor(Math.random() * 2);
+            if (num > 0) {
+                p1.fuelDecrease();
+                console.log(`${p1.name} fuel is ${p1.fuel}`);
+                console.log(`${p2.name} fuel is ${p2.fuel}`);
+                if (p1.fuel <= 0) {
+                    console.log(`${p1.name} has lost`);
+                    console.log(`${p2.name} has won`);
+                    process.exit();
+                }
+            }
+            if (num <= 0) {
+                p2.fuelDecrease();
+                console.log(`${p1.name} fuel is ${p1.fuel}`);
+                console.log(`${p2.name} fuel is ${p2.fuel}`);
+                if (p2.fuel <= 0) {
+                    console.log(`${p2.name} has lost`);
+                    console.log(`${p1.name} has won`);
+                    process.exit();
+                }
+            }
+        }
+        if (ask.opt == 'Surrender') {
+            console.log(`${p1.name} has surrendered`);
+            console.log(`${p2.name} has won`);
+            process.exit();
+        }
+    }
+    //Paul
+    if (opponet.select == 'Paul') {
+        let ask = await inquirer.prompt([
+            {
+                name: 'opt',
+                type: 'list',
+                message: 'What do you want to do?',
+                choices: ['Attack', 'Surrender']
+            }
+        ]);
+        if (ask.opt == 'Attack') {
+            let num = Math.floor(Math.random() * 2);
+            if (num > 0) {
+                p1.fuelDecrease();
+                console.log(`${p1.name} fuel is ${p1.fuel}`);
+                console.log(`${p2.name} fuel is ${p2.fuel}`);
+                if (p1.fuel <= 0) {
+                    console.log(`${p1.name} has lost`);
+                    console.log(`${p2.name} has won`);
+                    process.exit();
+                }
+            }
+            if (num <= 0) {
+                p2.fuelDecrease();
+                console.log(`${p1.name} fuel is ${p1.fuel}`);
+                console.log(`${p2.name} fuel is ${p2.fuel}`);
+                if (p2.fuel <= 0) {
+                    console.log(`${p2.name} has lost`);
+                    console.log(`${p1.name} has won`);
+                    process.exit();
+                }
+            }
+        }
+        if (ask.opt == 'Surrender') {
+            console.log(`${p1.name} has surrendered`);
+            console.log(`${p2.name} has won`);
+            process.exit();
+        }
+    }
+    //Lee
+    if (opponet.select == 'Lee') {
+        let ask = await inquirer.prompt([
+            {
+                name: 'opt',
+                type: 'list',
+                message: 'What do you want to do?',
+                choices: ['Attack', 'Surrender']
+            }
+        ]);
+        if (ask.opt == 'Attack') {
+            let num = Math.floor(Math.random() * 2);
+            if (num > 0) {
+                p1.fuelDecrease();
+                console.log(`${p1.name} fuel is ${p1.fuel}`);
+                console.log(`${p2.name} fuel is ${p2.fuel}`);
+                if (p1.fuel <= 0) {
+                    console.log(`${p1.name} has lost`);
+                    console.log(`${p2.name} has won`);
+                    process.exit();
+                }
+            }
+            if (num <= 0) {
+                p2.fuelDecrease();
+                console.log(`${p1.name} fuel is ${p1.fuel}`);
+                console.log(`${p2.name} fuel is ${p2.fuel}`);
+                if (p2.fuel <= 0) {
+                    console.log(`${p2.name} has lost`);
+                    console.log(`${p1.name} has won`);
+                    process.exit();
+                }
+            }
+        }
+        if (ask.opt == 'Surrender') {
+            console.log(`${p1.name} has surrendered`);
+            console.log(`${p2.name} has won`);
+            process.exit();
+        }
+    }
+} while (true);
